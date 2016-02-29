d6f59c962fe543a42b07182f850f69a83f4f916b
Davide Gatti <davide.gatti@kit.edu>
2016-02-29 10:40:18 +0100

    Optimized array numpy operations

--- postpro/psd.py
+++ postpro/psd.py
@@ -12,9 +12,12 @@
 
 # Inputs
 pl = 'uv'
-py = 10;
-utau = 1;
-nu = 200;
+py = 15;
+premultiply = True
+y_normalize = True
+scale = 'log'
+antisymm = True
+U=15.8956
 # ----------------
 from   dnsdata import *
 # ----------------
@@ -32,71 +35,91 @@ try:
     psd_x = np.load('psd_x.npy','r')
     psd_z = np.load('psd_z.npy','r')
     psd_y = np.load('psd_y.npy','r')
+    psd_1D_yx = np.load('psd_1D_yx.npy','r')
+    psd_1D_yz = np.load('psd_1D_yz.npy','r')
+    psd_1D_z = np.load('psd_1D_z.npy','r')
+    psd_1D_x = np.load('psd_1D_x.npy','r')
+    
 # Otherwise read results
 except IOError:
     with open ('psd.bin','r') as psd_file:
       psd = np.fromfile(psd_file,SPECTRUM)
-    psd = np.reshape(psd,(ny+3,nx+1,2*nz+1))
-    #psd = psd[1:ny+2] # throw away iy=-1 and iy=ny+1
+    psd = psd.reshape(ny+3,nx+1,2*nz+1)
 
 # Average top and bottom plane
-    for iy in range(iyd(int(ny/2))):
-      for uiuj in SPECTRUM.names:
-        psd[iy,:,:][uiuj] = 0.5*(psd[iy,:,:][uiuj]+psd[iyd(ny-iy),:,:][uiuj])
-    psd = psd[0:iyd(int(ny/2))+1]    
+    for uiuj in SPECTRUM.names:
+      if antisymm:
+        psd[1:iyd(int(ny/2)),:,:][uiuj] -= psd[ny+1:iyd(int(ny/2)):-1,:,:][uiuj]
+      else:
+        psd[1:iyd(int(ny/2)),:,:][uiuj] += psd[ny+1:iyd(int(ny/2)):-1,:,:][uiuj]
+      psd[1:iyd(int(ny/2)),:,:][uiuj] *= 0.5*(-1 if antisymm else 1)*alfa0*beta0
+    psd = psd[0:iyd(int(ny/2))+1]
     
-# Average the two nz half-planes
-    for iz in range(-nz,0):
-      for uiuj in SPECTRUM.names:
-        psd[:,:,izd(nz)-izd(iz)][uiuj] = 0.5*(psd[:,:,izd(iz)][uiuj]+psd[:,:,izd(nz)-izd(iz)][uiuj])
+# Sum the two nz half-planes
+    for uiuj in SPECTRUM.names:
+      psd[:,:,:][uiuj] += psd[:,:,::-1][uiuj]
+      psd[:,:,izd(nz)][uiuj] *= 0.5
     psd = psd[:,:,izd(0):izd(nz)+1]
-  
+    
+# Normalize (1-y/H)
+    if y_normalize:
+      for uiuj in SPECTRUM.names:
+        psd[:,:,:][uiuj] *= (1-y[0:iyd(int(ny/2))+1]).reshape(int(ny/2)+2,1,1)
+
 # Compute 1D spectra
     psd_x = np.zeros((int(ny/2)+2,nx+1),SPECTRUM)
     psd_z = np.zeros((int(ny/2)+2,nz+1),SPECTRUM)
     psd_y = np.zeros((nx+1,nz+1),SPECTRUM)
+    psd_1D_x = np.zeros(nx+1,SPECTRUM)
+    psd_1D_z = np.zeros(nz+1,SPECTRUM)
+    psd_1D_yx= np.zeros((int(ny/2)+2),SPECTRUM)
+    psd_1D_yz= np.zeros((int(ny/2)+2),SPECTRUM)
     for uiuj in SPECTRUM.names:
-      psd_x[:,:][uiuj] = beta0*np.sum(psd[:,:,:][uiuj],2)
-      psd_z[:,:][uiuj] = alfa0*np.sum(psd[:,:,:][uiuj],1)
-      for ix in range(0,nx+1):
-        for iz in range(0,nz+1):
-          psd_y[ix,iz][uiuj] = np.trapz(psd[iyd(0):,ix,iz][uiuj],y[iyd(0):iyd(int(ny/2))+1])
-
+      psd_z[:,:][uiuj] = psd[:,0,:][uiuj]+2*np.sum(psd[:,1:,:][uiuj],1)
+      psd_x[:,:][uiuj] = np.sum(psd[:,:,:][uiuj],2)
+      psd_1D_yx[:][uiuj]= psd_x[:,0][uiuj]+2*np.sum(psd_x[:,1:][uiuj],1)
+      psd_1D_yz[:][uiuj]= np.sum(psd_z[:,:][uiuj],1)
+      psd_1D_z[:][uiuj] = np.trapz(psd_z[iyd(0):,:][uiuj],y[iyd(0):iyd(int(ny/2))+1],axis=0)
+      psd_1D_x[:][uiuj] = np.trapz(psd_x[iyd(0):,:][uiuj],y[iyd(0):iyd(int(ny/2))+1],axis=0)
+      psd_y[:,:][uiuj] = np.trapz(psd[iyd(0):,:,:][uiuj],y[iyd(0):iyd(int(ny/2))+1],axis=0)
+      
 # Premultiply spectra
-    for uiuj in SPECTRUM.names:
-      for ix in range(0,nx+1):
-        psd_x[:,ix][uiuj] = kx[ix]*psd_x[:,ix][uiuj]
-        psd[:,ix,:][uiuj] = kx[ix]*psd[:,ix,:][uiuj]
-        psd_y[ix,:][uiuj] = kx[ix]*psd_y[ix,:][uiuj]
-      for iz in range(0,nz+1):
-        psd_z[:,iz][uiuj] = kz[iz]*psd_z[:,iz][uiuj]
-        psd[:,:,iz][uiuj] = kz[iz]*psd[:,:,iz][uiuj]
-        psd_y[:,iz][uiuj] = kz[iz]*psd_y[:,iz][uiuj]
+    if premultiply:
+      for uiuj in SPECTRUM.names:
+        for ix in range(0,nx+1):
+          psd_x[:,ix][uiuj] = kx[ix]*psd_x[:,ix][uiuj]
+          psd[:,ix,:][uiuj] = kx[ix]*psd[:,ix,:][uiuj]
+          psd_y[ix,:][uiuj] = kx[ix]*psd_y[ix,:][uiuj]
+        for iz in range(0,nz+1):
+          psd_z[:,iz][uiuj] = kz[iz]*psd_z[:,iz][uiuj]
+          psd[:,:,iz][uiuj] = kz[iz]*psd[:,:,iz][uiuj]
+          psd_y[:,iz][uiuj] = kz[iz]*psd_y[:,iz][uiuj]
 
 # Save the spectra
-    for outdata in ('psd','psd_x','psd_z','psd_y'):
+    for outdata in ('psd','psd_x','psd_z','psd_y','psd_1D_yx','psd_1D_yz','psd_1D_z','psd_1D_x'):
       np.save(outdata,eval(outdata)) 
       
-# Plot 2D spectra
-plt.figure()
-plt.contourf(2*pi/kz[1:nz+1],2*pi/kx[1:nx+1],psd[py,1:nx+1,1:nz+1][pl])
-plt.xscale('log'); plt.yscale('log')
-plt.xlabel('$k_x h$'); plt.ylabel('$k_z h$'); plt.title(pl+' power spectrum at height '+'{:.2}'.format(y[py])+' $y/h$')
+# uv - y
+f, ax = plt.subplots(2, 3)      
+ax[0,0].plot(psd_1D_yx[iyd(0):iyd(int(ny/2))+1][pl]/np.power(U,2),y[iyd(0):iyd(int(ny/2))+1])
+ax[0,0].plot(psd_1D_yz[iyd(0):iyd(int(ny/2))+1][pl]/np.power(U,2),y[iyd(0):iyd(int(ny/2))+1])
+ax[0,0].set_yscale(scale)
+# uv - lambdax
+ax[0,1].contourf(2*pi/kx[1:nx+1],y[iyd(1):iyd(int(ny/2))+1],psd_x[iyd(1):,1:nx+1][pl]/np.power(U,2))
+ax[0,1].set_xscale(scale); ax[0,1].set_yscale(scale)
+# uv - lambdaz
+ax[0,2].contourf(2*pi/kz[1:nz+1],y[iyd(1):iyd(int(ny/2))+1],psd_z[iyd(1):,1:nz+1][pl]/np.power(U,2))
+ax[0,2].set_xscale(scale); ax[0,1].set_yscale(scale)
+# uv - x
+ax[1,1].plot(2*pi/kx[1:nx+1],psd_1D_x[1:nx+1][pl]/np.power(U,2))
+ax[1,1].set_xscale(scale)
+# uv - z
+ax[1,2].plot(2*pi/kz[1:nx+1],psd_1D_z[1:nx+1][pl]/np.power(U,2))
+ax[1,2].set_xscale(scale)
 
-# Plot 2D spectra (y-integrated)
-plt.figure()
-plt.contourf(2*pi/kz[1:nz+1]*1000,2*pi/kx[1:nx+1]*1000,psd_y[1:nx+1,1:nz+1][pl])
-plt.xscale('log'); plt.yscale('log')
-plt.xlabel('$k_x h$'); plt.ylabel('$k_z h$'); plt.title('Power spectrum of the y-integral of '+pl)
+print(6*(psd_1D_x[0][pl]+2*np.sum(psd_1D_x[1:nx+1][pl]))/np.power(U,2))
+print(6*np.sum(psd_1D_z[0:nz+1][pl]/np.power(U,2)))
+print(6*np.trapz(psd_1D_yx[iyd(0):iyd(int(ny/2))+1][pl]/np.power(U,2),y[iyd(0):iyd(int(ny/2))+1]))
+print(6*np.trapz(psd_1D_yz[iyd(0):iyd(int(ny/2))+1][pl]/np.power(U,2),y[iyd(0):iyd(int(ny/2))+1]))
 
-# Plot 1D spectra
-plt.figure()
-plt.contourf(2*pi/kx[1:nx+1],y[iyd(1):iyd(int(ny/2))+1],psd_x[iyd(1):,1:nx+1][pl])
-plt.xscale('log'); plt.yscale('log')
-plt.ylabel('$y/ h$'); plt.xlabel('$k_x h$'); plt.title(pl+' 1D power spectrum')
-
-plt.figure()
-plt.contourf(2*pi/kz[1:nz+1],y[iyd(1):iyd(int(ny/2))+1],psd_z[iyd(1):,1:nz+1][pl])
-plt.xscale('log'); plt.yscale('log')
-plt.ylabel('$y/ h$'); plt.xlabel('$k_z h$'); plt.title(pl+' 1D power spectrum')
-plt.show()
+plt.show()      
